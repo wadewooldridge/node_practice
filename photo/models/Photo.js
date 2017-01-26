52284462dcd2feba5fc531541869d3826e0d31aa
wadewooldridge <waderwooldridge@gmail.com>
2017-01-26 13:25:24 -0800

    Basic upload working with busboy.

---
+++ photo/models/Photo.js
@@ -0,0 +1,10 @@
+/* Mongoose database model for photos. */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/photo_app');
+
+var schema = new mongoose.Schema({
+    name:   String,
+    path:   String
+});
+
+module.exports = mongoose.model('Photo', schema);
